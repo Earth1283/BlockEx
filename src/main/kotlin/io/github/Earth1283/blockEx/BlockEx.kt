21c4352bc7a17e01d7a0673aa8d3fce40ecd9d7e
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-05-01 08:15:21 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/Earth1283/blockEx/BlockEx.kt
@@ -0,0 +1,14 @@
+package io.github.Earth1283.blockEx
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BlockEx : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/io/github/Earth1283/blockEx/BlockExLoader.kt
@@ -0,0 +1,11 @@
+package io.github.Earth1283.blockEx
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder
+import io.papermc.paper.plugin.loader.PluginLoader
+
+internal class BlockExLoader : PluginLoader {
+
+    override fun classloader(builder: PluginClasspathBuilder) {
+        // Add dynamically loaded libraries here
+    }
+}
